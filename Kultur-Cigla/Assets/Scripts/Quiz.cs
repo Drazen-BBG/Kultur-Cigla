534f855b8329b2ddc24e626db98998744841a459
Drazen Drinic <drazendrinic@hotmail.com>
2021-10-14 23:02:24 +0200

    Buttons Method 1/2

--- Kultur-Cigla/Assets/Scripts/Quiz.cs
+++ Kultur-Cigla/Assets/Scripts/Quiz.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI questionText;
     [SerializeField] private QuestionsSO question;
     [SerializeField] private GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] private Sprite defaultAnswerSprite;
+    [SerializeField] private Sprite correcttAnswerSprite;
 
     void Start()
     {
@@ -21,5 +25,17 @@ public class Quiz : MonoBehaviour
         }
     }
 
-
+    public void OnAnswerSelected(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct";
+            answerButtons[index].GetComponentInChildren<Image>().sprite = correcttAnswerSprite;
+        }
+        else
+        {
+            questionText.text = "Sorry, the correct answer is " + question.GetAnswer(question.GetCorrectAnswerIndex());
+            answerButtons[question.GetCorrectAnswerIndex()].GetComponentInChildren<Image>().sprite = correcttAnswerSprite;
+        }
+    }
 }
