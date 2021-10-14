82c5655575b7608a942502704f073bd5b9339e87
Drazen Drinic <drazendrinic@hotmail.com>
2021-10-14 18:23:35 +0200

    Answers in SO

--- Kultur-Cigla/Assets/Scripts/QuestionsSO.cs
+++ Kultur-Cigla/Assets/Scripts/QuestionsSO.cs
@@ -2,15 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Quiz Question", fileName ="New Question")]
+[CreateAssetMenu(menuName = "Quiz Question", fileName ="Question")]
 public class QuestionsSO : ScriptableObject
 {
+    //Question
     [TextArea(2,6)]
     [SerializeField] 
     string question = "Enter new question text here";
+
+    //Answers
+    [SerializeField]
+    string[] answers = new string[4];
+    [SerializeField]
+    int correctAnswerIndex;
+
+
+    //Question categories 
+    [SerializeField]
+    bool sport, history, geography, music, science;
     
+
+
+    //Getter Methods 
     public string GetQuestion()
     {
         return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetCorrectAnswer(int index)
+    {
+        return answers[index];
     }
 }
