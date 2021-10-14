21da93b66651666db7ba0a34807fcaa64cce2043
Drazen Drinic <drazendrinic@hotmail.com>
2021-10-15 00:15:19 +0200

    Buttons Method 2/2
    All methods are cleaned and rearranged and all working for now. Let keep coding

--- Kultur-Cigla/Assets/Scripts/Quiz.cs
+++ Kultur-Cigla/Assets/Scripts/Quiz.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class Quiz : MonoBehaviour
 {
@@ -15,9 +16,13 @@ public class Quiz : MonoBehaviour
 
     void Start()
     {
-        questionText.text = question.GetQuestion();
+        DisplayQuestion();
+        //GetNextQuestion();
+    }
 
-        
+    private void DisplayQuestion()
+    {
+        questionText.text = question.GetQuestion();
 
         for (int i = 0; i < answerButtons.Length; i++)
         {
@@ -30,12 +35,37 @@ public class Quiz : MonoBehaviour
         if (index == question.GetCorrectAnswerIndex())
         {
             questionText.text = "Tačno!";
-            answerButtons[index].GetComponentInChildren<Image>().sprite = correcttAnswerSprite;
+            answerButtons[index].GetComponent<Image>().sprite = correcttAnswerSprite;
         }
         else
         {
             questionText.text = "Žao mi je, tačan odgovor je: \n" + question.GetAnswer(question.GetCorrectAnswerIndex());
-            answerButtons[question.GetCorrectAnswerIndex()].GetComponentInChildren<Image>().sprite = correcttAnswerSprite;
+            answerButtons[question.GetCorrectAnswerIndex()].GetComponent<Image>().sprite = correcttAnswerSprite;
+        }
+
+        SetButtonState(false);
+    }
+
+    private void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+
+    private void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            answerButtons[i].GetComponent<Button>().interactable = state;
+        }
+    }
+
+    private void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            answerButtons[i].GetComponent<Image>().sprite = defaultAnswerSprite;
         }
     }
 }
