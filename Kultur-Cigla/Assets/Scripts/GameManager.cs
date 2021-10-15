48cc3700cede090ace1121fbe0bf77ea4e7af428
Drazen Drinic <drazendrinic@hotmail.com>
2021-10-15 23:16:40 +0200

    GameManager not giving errors, need to add button functionality

--- Kultur-Cigla/Assets/Scripts/GameManager.cs
+++ Kultur-Cigla/Assets/Scripts/GameManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Quiz quiz;
+    EndScript endScript;
+
+    void Awake()
+    {
+        quiz = FindObjectOfType<Quiz>();
+        endScript = FindObjectOfType<EndScript>();
+    }
     void Start()
     {
-        
+        quiz.gameObject.SetActive(true);
+        endScript.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (quiz.isComplete)
+        {
+            quiz.gameObject.SetActive(false);
+            endScript.gameObject.SetActive(true);
+            endScript.ShowFinalText();
+        }
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
     }
 }
